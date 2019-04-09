09ab2c0d561a7b4ca1a6249e84fd8daa2065f8a9
rizkavio21 <rizkavio21@gmail.com>
2019-04-09 23:19:42 +0700

    tubes webpro

---
+++ application/views/Utama/One.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>HelloMotion.com</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="assets/back.css">
+  <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
+  <style type="text/css">
+    body{
+    background-color: #1b1818;
+    font-family: 'Montserrat' , sans-serif;
+    font-weight: bolder;
+    }
+
+    .navbar{
+      background-color: #1aaed0;
+
+    }
+    .nav-link{
+      color: #FFFFFF;
+      position: relative;
+      font-weight: italic;
+    }
+    .nav-link:hover {
+    color: #FFFF00;
+    }
+    .button{
+      background-color: grey;
+      border: none;
+      text-align: center;
+      text-decoration: none;
+      display: inline-block;
+      font-size: 16px;
+      cursor: pointer;
+      top: 70px;
+    }
+    
+  </style>
+</head>
+<body>
+  <div class="navbar">
+    <ul class="navbar navbar-default navbar-fixed-top">
+      <li class="navbar-brand" href="#">
+        <img src="assets/logo.png">
+      </li>
+        <a class="nav-link" href="#">PARTISIPASI</a>
+        <a class="nav-link" href="#">BLOG</a>
+        <a class="nav-link" href="#">ACADEMY</a>
+        <a class="nav-link" href="#">HELLO FEST</a>
+        <button style="background-color: grey; border: none;">
+            <input type="submit" class="button" value="Daftar!" />
+            <input type="submit" class="button" value="Login"/>
+        </button>
+      </li>
+    </ul>
+  </div>
+
+</body>
+</html>
